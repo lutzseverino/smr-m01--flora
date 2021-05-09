8eae124ba8e88992e1064c5e842f025699e275cc
Jasper Lutz Severino <jasperworld9@gmail.com>
2021-05-09 18:22:31 +0200

    Rename Bot.java to BotValues.java
    Signed-off-by: Jasper Lutz Severino <jasperworld9@gmail.com>

---
+++ src/main/java/me/jasperedits/docs/BotValues.java
@@ -0,0 +1,33 @@
+package me.jasperedits.docs;
+
+import lombok.Getter;
+
+@Getter
+public class BotValues {
+
+    public BotValues(String token, String defaultLanguage) {
+        this.token = token;
+        this.defaultLanguage = defaultLanguage;
+    }
+
+    public BotValues() {}
+
+    @Getter
+    private String token;
+    @Setter
+    @Getter
+    private String defaultLanguage;
+
+    // Database settings
+
+    private String databaseHostname;
+
+    private int databasePort;
+
+    private String databaseUsername;
+
+    private String databasePassword;
+
+    private String databaseName;
+
+}
