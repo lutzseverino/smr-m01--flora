badd6156d7ffe75f89c6eb198d1c02052d5e4df3
Jasper Lutz Severino <jasperworld9@gmail.com>
2021-05-09 18:24:43 +0200

    Create first unfinished MongoDB document
    Signed-off-by: Jasper Lutz Severino <jasperworld9@gmail.com>

---
+++ src/main/java/me/jasperedits/docs/Guild.java
@@ -0,0 +1,21 @@
+package me.jasperedits.docs;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.mongojack.Id;
+
+
+@Getter
+@Setter
+public class Guild implements Model {
+
+    @Id
+    private String id;
+
+    /*
+    If a Discord guild sent 1,000,000,000 messages per second,
+    it would take roughly 292471 years to reach the long primitive limit.
+     */
+    private long messageCount;
+
+}
