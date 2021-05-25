62f54b24ed4f310d7330933f64ca01519fca9006
Jasper Lutz Severino <jasperworld9@gmail.com>
2021-05-25 09:19:36 +0200

    Create Info.java to prepare for upcoming changes
    Signed-off-by: Jasper Lutz Severino <jasperworld9@gmail.com>

---
+++ src/main/java/me/jasperedits/commands/impl/Info.java
@@ -0,0 +1,20 @@
+package me.jasperedits.commands.impl;
+
+import me.jasperedits.commands.Command;
+import me.jasperedits.commands.CommandInformation;
+import me.jasperedits.commands.CommandType;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+@CommandType(names = "prefix")
+public class Info implements Command {
+    GuildMessageReceivedEvent event;
+    Command command;
+
+    public Info() {
+    }
+
+    @Override
+    public void execute(CommandInformation command) {
+        command.getEvent().getChannel().sendMessage("test").queue();
+    }
+}
