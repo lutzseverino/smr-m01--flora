a3e110aa4da788344ee4eb446dfec1c537d65fb7
Jasper Lutz Severino <jasperworld9@gmail.com>
2021-05-25 14:21:44 +0200

    Add permission checking
    Signed-off-by: Jasper Lutz Severino <jasperworld9@gmail.com>

--- src/main/java/me/jasperedits/commands/CommandService.java
+++ src/main/java/me/jasperedits/commands/CommandService.java
@@ -3,8 +3,11 @@ package me.jasperedits.commands;
 import lombok.SneakyThrows;
 import me.jasperedits.docs.GuildDAO;
 import me.jasperedits.docs.impl.Guild;
+import me.jasperedits.embeds.EmbedType;
+import me.jasperedits.embeds.EmbedTemplate;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -17,7 +20,7 @@ public class CommandService extends ListenerAdapter {
     @SneakyThrows
     @Override
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
-        String message = event.getMessage().getContentRaw();
+        Message message = event.getMessage();
         Member member = event.getMember();
         Guild guild = GuildDAO.getGuild(event.getGuild().getId());
         String prefix = guild.getPrefix();
@@ -27,13 +30,24 @@ public class CommandService extends ListenerAdapter {
         }
 
         for (Command command : CommandRegistry.getAllCommands()) {
-            for (String alias : command.getClass().getAnnotation(CommandType.class).names()) {
-                if (message.equals(prefix + alias)) {
-                    List<String> args = new ArrayList<>(Arrays.asList(message.split(" ")));
-                    args.remove(0);
+            if (member.hasPermission(command.getClass().getAnnotation(CommandType.class).permission())) {
+                for (String alias : command.getClass().getAnnotation(CommandType.class).names()) {
+                    if (message.getContentRaw().equals(prefix + alias)) {
+                        List<String> args = new ArrayList<>(Arrays.asList(message.getContentRaw().split(" ")));
+                        args.remove(0);
 
-                    CommandRegistry.byName(alias).execute(new CommandInformation(args, event, guild));
+                        CommandRegistry.byName(alias).execute(new CommandInformation(args, event, guild));
+                    }
                 }
+
+            } else {
+                String PERMISSIONS_TITLE = "Unauthorized action";
+                String PERMISSIONS_DESC = "You don't have enough permissions to call that command.";
+
+                EmbedBuilder embedBuilder = new EmbedTemplate(EmbedType.ERROR, member.getUser()).getEmbedBuilder();
+                embedBuilder.setTitle("**__" + PERMISSIONS_TITLE + "__**");
+                embedBuilder.setDescription(PERMISSIONS_DESC);
+                message.reply(embedBuilder.build()).queue();
             }
         }
     }
