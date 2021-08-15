15374f4670f958529235e4119436eeb8b8ba9f70
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2021-08-16 01:31:23 +0200

    Added Allowance factors

---
+++ src/main/java/me/jasperedits/command/rule/impl/Allowance.java
@@ -0,0 +1,90 @@
+package me.jasperedits.command.rule.impl;
+
+import me.jasperedits.FloraBot;
+import me.jasperedits.command.CommandInformation;
+import me.jasperedits.command.annotation.CommandType;
+import me.jasperedits.command.rule.Rule;
+import me.jasperedits.command.rule.RuleType;
+import me.jasperedits.command.settings.CommandAllowance;
+import me.jasperedits.command.settings.CommandFormat;
+import me.jasperedits.embed.EmbedFormat;
+import me.jasperedits.embed.EmbedTemplate;
+import me.jasperedits.manager.Language;
+import me.jasperedits.util.DiscordUtil;
+import net.dv8tion.jda.api.EmbedBuilder;
+
+@RuleType()
+public class Allowance implements Rule {
+    @Override
+    public boolean check(CommandFormat format, CommandType type, CommandInformation information) {
+        if (type.allowance().equals(CommandAllowance.EVERYTHING)) return true;
+
+        switch (format) {
+            case LEGACY -> {
+                switch (type.allowance()) {
+                    case CHAT_DM -> {
+                        return !information.getLegacyEvent().isFromGuild();
+                    }
+                    case CHAT_GUILD -> {
+                        return information.getLegacyEvent().isFromGuild();
+                    }
+                    case SERVER_OWNER -> {
+                        return information.getLegacyEvent().getGuild().getOwner() != null
+                                && information.getLegacyEvent().getMember().equals(information.getLegacyEvent().getGuild().getOwner());
+                    }
+                    case BOT_OWNER -> {
+                        return information.getLegacyEvent().getMember().getIdLong() == FloraBot.getInstance().getBotValues().getOwner();
+                    }
+                }
+            }
+
+            case INTERACTIVE -> {
+                switch (type.allowance()) {
+                    case CHAT_DM -> {
+                        return !information.getInteractionEvent().isFromGuild();
+                    }
+                    case CHAT_GUILD -> {
+                        return information.getInteractionEvent().isFromGuild();
+                    }
+                    case SERVER_OWNER -> {
+                        return information.getInteractionEvent().getGuild().getOwner() != null
+                                && information.getInteractionEvent().getMember().equals(information.getInteractionEvent().getGuild().getOwner());
+                    }
+                    case BOT_OWNER -> {
+                        return information.getInteractionEvent().getMember().getIdLong() == FloraBot.getInstance().getBotValues().getOwner();
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void legacyOutput(CommandInformation information) {
+        // Get guild's language.
+        Language language = information.getGuild().getLanguage();
+
+        // Build the output embed.
+        EmbedBuilder output = new EmbedTemplate(EmbedFormat.DEFAULT, information.getLegacyEvent().getAuthor()).getEmbedBuilder();
+        output.setTitle(language.getValue("error.allowance.title"));
+        output.setDescription(language.getValue("error.allowance.description"));
+
+        // Send the error message.
+        information.getLegacyEvent().getMessage().reply(output.build()).mentionRepliedUser(false).queue();
+    }
+
+    @Override
+    public void interactionOutput(CommandInformation information) {
+        // Get guild's language.
+        Language language = information.getGuild().getLanguage();
+
+        // Build the output embed.
+        EmbedBuilder output = new EmbedTemplate(EmbedFormat.DEFAULT, information.getInteractionEvent().getUser()).getEmbedBuilder();
+        output.setTitle(language.getValue("error.allowance.title"));
+        output.setDescription(language.getValue("error.allowance.description"));
+
+        // Send the error message.
+        information.getInteractionEvent().replyEmbeds(output.build()).addActionRow(DiscordUtil.addWikiComponent(language))
+                .setEphemeral(true).queue();
+    }
+}
---
+++ src/main/java/me/jasperedits/command/settings/CommandAllowance.java
@@ -0,0 +1,9 @@
+package me.jasperedits.command.settings;
+
+public enum CommandAllowance {
+    CHAT_DM,
+    BOT_OWNER,
+    SERVER_OWNER,
+    CHAT_GUILD,
+    EVERYTHING
+}
