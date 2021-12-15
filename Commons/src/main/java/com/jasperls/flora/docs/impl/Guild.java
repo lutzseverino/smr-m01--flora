a1b6b00638137a155fe6adbd0e4948089686252e
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2021-12-16 00:32:57 +0100

    Create Guild database object and test database framework

---
+++ Commons/src/main/java/com/jasperls/flora/docs/impl/Guild.java
@@ -0,0 +1,26 @@
+package com.jasperls.flora.docs.impl;
+
+import com.google.common.collect.Maps;
+import com.jasperls.flora.docs.SimpleDocument;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Getter
+@Setter
+public class Guild extends SimpleDocument {
+
+    private String guildId;
+    private long messageCount;
+    private long completedObjectiveCount;
+    private Map<String, Boolean> ongoingObjectives = Maps.newHashMap();
+    private long channelId = 0;
+
+    public Guild() {
+    }
+
+    public Guild(String id) {
+        this.guildId = id;
+    }
+}
--- Discord/src/main/java/com/jasperls/flora/discord/Flora.java
+++ Discord/src/main/java/com/jasperls/flora/discord/Flora.java
@@ -1,6 +1,8 @@
 package com.jasperls.flora.discord;
 
+import com.jasperls.flora.config.DatabaseConfig;
 import com.jasperls.flora.config.Values;
+import com.jasperls.flora.database.MongoDatabaseManager;
 import com.jasperls.flora.logger.Log;
 import com.jasperls.flora.yaml.Snakelet;
 import net.dv8tion.jda.api.entities.Activity;
@@ -18,6 +20,16 @@ public class Flora {
         builder.setBulkDeleteSplittingEnabled(false);
 
         String status = values.getBotConfig().getStatus();
+
+        DatabaseConfig databaseConfig = values.getDbConfig();
+
+        new MongoDatabaseManager(
+                databaseConfig.getHost(),
+                databaseConfig.getPort(),
+                databaseConfig.getUser(),
+                databaseConfig.getPasswd(),
+                databaseConfig.getName()
+        );
 
         switch (values.getBotConfig().getActivity()) {
             case "competing" -> builder.setActivity(Activity.competing(status));
