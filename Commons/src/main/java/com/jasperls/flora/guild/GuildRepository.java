1badc6bb20fb6dc3cd19056518839e6bfdf6f085
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2021-12-18 20:41:41 +0100

    Create a Guild data access object

---
+++ Commons/src/main/java/com/jasperls/flora/guild/GuildRepository.java
@@ -0,0 +1,9 @@
+package com.jasperls.flora.guild;
+
+import com.jasperls.flora.docs.impl.Guild;
+
+import java.util.concurrent.ExecutionException;
+
+public interface GuildRepository {
+    Guild get(String id) throws ExecutionException;
+}
---
+++ Commons/src/main/java/com/jasperls/flora/guild/GuildRepositoryImpl.java
@@ -0,0 +1,44 @@
+package com.jasperls.flora.guild;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.jasperls.flora.database.DatabaseCollection;
+import com.jasperls.flora.database.DatabaseManager;
+import com.jasperls.flora.docs.impl.Guild;
+import lombok.NonNull;
+
+import javax.inject.Inject;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+public class GuildRepositoryImpl implements GuildRepository {
+
+    private final DatabaseCollection<Guild> guildCollection;
+    private final LoadingCache<String, Guild> cache = CacheBuilder.newBuilder()
+            .expireAfterAccess(10, TimeUnit.MINUTES)
+            .build(
+                    new CacheLoader<>() {
+                        @Override
+                        public @NonNull Guild load(@NonNull String id) {
+                            return guildCollection.get(id).orElseGet(() -> new Guild(id));
+                        }
+                    });
+
+    @Inject
+    public GuildRepositoryImpl(DatabaseManager databaseManager) {
+        this.guildCollection = databaseManager.getCollection(Guild.class);
+    }
+
+    @Override
+    public Guild get(String id) {
+        try {
+            return cache.get(id);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+}
