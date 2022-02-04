3d868b33931e4619cde8e70c496bd495b1ea49e5
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-02-04 13:07:34 +0100

    Create language system

---
+++ Commons/src/main/java/com/jasperls/flora/language/Dictionary.java
@@ -0,0 +1,42 @@
+package com.jasperls.flora.language;
+
+import lombok.Getter;
+
+import java.io.File;
+import java.util.*;
+
+public class Dictionary {
+    @Getter
+    private final ResourceBundle bundle;
+
+    public Dictionary(Locale language) {
+        bundle = ResourceBundle.getBundle("language.bundle", language);
+    }
+
+    public Dictionary() {
+        bundle = ResourceBundle.getBundle("language.bundle");
+    }
+
+    public String getString(String key) {
+        return bundle.getString(key);
+    }
+
+    public ArrayList<String> getAvailableLanguages() {
+        File languageFolder = new File(Objects.requireNonNull(this.getClass().getResource("/language/")).getPath());
+        ArrayList<String> languages = new ArrayList<>();
+
+        Arrays.stream(Objects.requireNonNull(languageFolder.listFiles())).forEach(file -> {
+            if (!file.getName().matches(".*[a-z]{2}_[A-Z]{2}.*")) {
+                languages.add("en_US");
+                return;
+            }
+            languages.add(file.getName().replaceAll("(bundle_)|(.properties)", ""));
+        });
+
+        return languages;
+    }
+
+    public boolean isLanguageAvailable(String languageCode) {
+        return getAvailableLanguages().contains(languageCode);
+    }
+}
---
+++ Discord/src/main/java/com/jasperls/flora/discord/commands/impl/Language.java
@@ -0,0 +1,53 @@
+package com.jasperls.flora.discord.commands.impl;
+
+import com.jasperls.flora.discord.commands.CommandInfo;
+import com.jasperls.flora.discord.design.Colors;
+import com.jasperls.flora.discord.design.embed.EmbedTemplate;
+import com.jasperls.flora.docs.impl.Guild;
+import com.jasperls.flora.guild.GuildRepositoryImpl;
+import com.jasperls.flora.language.Dictionary;
+import com.jasperls.rimor.annotation.MethodCommand;
+import com.jasperls.rimor.jda.annotation.OptionDetails;
+import com.jasperls.rimor.jda.data.OptionExecutionData;
+import com.jasperls.rimor.jda.type.OptionSubcommand;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+
+import javax.inject.Inject;
+import java.util.Locale;
+
+public class Language extends OptionSubcommand {
+    @Inject
+    private GuildRepositoryImpl guildRepository;
+
+    private String code;
+
+    @MethodCommand
+    public void run(CommandInfo info) {
+        Guild guild = info.getGuild();
+        String[] newLanguage = this.code.split("_");
+
+        Locale newLocale = new Locale(newLanguage[0], newLanguage[1]);
+        EmbedBuilder out = new EmbedTemplate(Colors.SUCCESS).getBuilder();
+
+        info.getEvent().deferReply().queue(response -> {
+            info.getGuild().setLanguage(newLocale);
+            guildRepository.update(guild);
+
+            Dictionary language = new Dictionary(info.getGuild().getLanguage());
+
+            out.setTitle(language.getString("commons.title.data_updated"));
+            out.setDescription(String.format(language.getString("config.language.description.success"), "`" + newLocale + "`"));
+
+            response.sendMessageEmbeds(out.build()).queue();
+        });
+    }
+
+    @OptionDetails(
+            type = OptionType.STRING,
+            position = 1
+    )
+    public void code(OptionExecutionData optionData) {
+        this.code = optionData.getOptionMapping().getAsString();
+    }
+}
